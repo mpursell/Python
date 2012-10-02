8c1704f55a25b6c8e0512f7d36cbb50b88bbcd99
mpursell <michael.pursell@gmail.com>
2012-10-02 18:42:55 +0100

    Start of log file scripts folder

---
+++ log file scripts/mergelogs.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#
+#Script to merge a directory of log files into one log file
+#Note, make sure the log files are in the right format.  The script will just append the data!
+#Michael Pursell 2012
+
+import sys, os, subprocess
+
+def file_ops(log, user_input, dir_list):
+    x=1
+    try:
+        for arg in dir_list:
+            file = open(user_input+'\\'+dir_list[x], 'r') #look at filename in dir_list[x] position
+            read_file = file.read()
+            logfile = open(log, 'a')
+            logfile.write(read_file+'\n')
+            x=x+1 #increment x to iterate over dir_list
+        logfile.close
+    except IndexError:  #if x should increase too much, catch and exit gracefully
+            exit
+    
+        
+
+
+def user_input(command):
+
+    #grab the file names from the user and call file_ops to read / write / close
+    log = input('Enter log file to append:  ')
+    subprocess.call(command, shell=True)
+    user_input = input('Gimme a directory to parse:   ')
+    subprocess.call(command, shell=True)
+    dir_list = os.listdir(user_input) #get the files in the directory as a list
+
+    file_ops(log, user_input, dir_list) #call file_ops
+     
+
+def main():
+
+    command = 'cls'
+    choice = input('Append file? (Y/N): ')
+    subprocess.call(command, shell=True)
+
+    if choice == 'y':
+        user_input(command)
+        subprocess.call(command, shell=True)
+        print('Success! Contents appended and log file closed')
+    elif choice == 'n':
+        print('Exiting')
+        exit
+    else:
+        print('You need to choose yes or no!')
+        exit
+
+
+if __name__ == "__main__":
+    main()
---
+++ log file scripts/readme.txt
@@ -0,0 +1,4 @@
+Scripts to manipulate log files:
+
+mergelogs.py - Takes a new file name and a directory to parse as inputs.  Script then parses the directory and merges all the files
+		in that directory into the new file.
