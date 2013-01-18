a3dd4e07b4c25ad4e9131a6e60a4a397f8baae22
mpursell <michael.pursell@gmail.com>
2013-01-18 13:20:24 +0000

    Update windows_updates.py

--- windows_updates.py
+++ windows_updates.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env PYTHON
-#script to query the Windows hotfixes installed, then scrape the titles from the MS KnowledgeBase
-#Output a csv with the results
-#Tested with Python 2.7 under Windows 7
-#Michael Pursell 2013
+#	Script to query the Windows hotfixes installed, then scrape the titles from the MS KnowledgeBase
+#	Output a csv with the results
+#	Tested with Python 2.7 under Windows 7
+#	Michael Pursell 2013
 ##############################################################################################################
 
 #import subprocess, urllib, re, string, sys, os
@@ -12,48 +12,41 @@ from urllib import urlopen
 from re import findall
 from string import lstrip
 
+
 	
 #function to setup some initial parameters and the WMI query	
 def runQuery():
 	
-	call("cls", shell=True) #clear the terminal
-	print("\n********************************************************************************")
-	print("\nThis script will find the Hotfix IDs and the associated MS KnowledgeBase titles")
-	print("Please make sure that you have a working internet connection!\n")
-	question = raw_input("Proceed? Y/N...  ")
 	
+	#shell out the wmi query to output the hotifx ids and URLs to text file
+	call("wmic qfe >> c:\updates.txt", shell=True)
 	
-	if question == "y":
-	
-		#shell out the wmi query to output the hotifx ids and URLs to text file
-		call("wmic qfe >> c:\updates.txt", shell=True)
+	#set the input file and initial regex searches up to find the URLs and the KB ids from the txt file
+	with open("c:\updates.txt", 'rb') as inputFile:
 		
-		#set the input file and initial regex searches up to find the URLs and the KB ids from the txt file
-		with open("c:\updates.txt", 'rb') as inputFile:
+		#read in the file and convert it to utf-16,
+		#required to avoid the wide charset that the wmic query outputs
+		readFile = inputFile.read()
+		convertedFile = readFile.decode('utf-16') 
+		
+		#regex for URLs - in this case all the URLs follow this pattern: http://support.microsoft.com/?kbid=2425227
+		urlSearch = findall("\S+[=]\d\d\d\d+\s", convertedFile) 
+		
+		updateIDSearch = findall("KB\d+\s",convertedFile) #regex for KB ID
+		
 			
-			#read in the file and convert it to utf-16,
-			#required to avoid the wide charset that the wmic query outputs
-			readFile = inputFile.read()
-			convertedFile = readFile.decode('utf-16') 
-			
-			#regex for URLs - in this case all the URLs follow this pattern: http://support.microsoft.com/?kbid=2425227
-			urlSearch = findall("\S+[=]\d\d\d\d+\s", convertedFile) 
-			
-			updateIDSearch = findall("KB\d+\s",convertedFile) #regex for KB ID
-			
-				
-			#call search function
-			searchHTML(convertedFile, urlSearch, updateIDSearch) 
-			
-			call("cls", shell=True) #Clear the terminal
-			print("Completed! Check the root of C: for the updatesOutput.csv")
-			#exit when done
-			exit()
-	else:
+		#call search function
+		searchHTML(convertedFile, urlSearch, updateIDSearch) 
+		
+		call("cls", shell=True) #Clear the terminal
+		print("Completed! Check the root of C: for the updatesOutput.csv")
+		#exit when done
 		exit()
 		
-		
+		#return the values for searchHTML() to use
+		return(convertedFile, urlSearch, updateIDSearch)
 
+		
 #function to open the URLs, search the HTMl and pull content together into the output file. 
 def searchHTML(convertedFile, urlSearch, updateIDSearch):
 	
@@ -87,18 +80,32 @@ def searchHTML(convertedFile, urlSearch, updateIDSearch):
 		#dictionary that zips together the list of titles with KB ids
 		updateDict = dict(zip(updateTitleList,updateIDSearch))
 		
-		
 		#iterate over the dictionary and write keys and values to the output file
 		for key, value in updateDict.iteritems():
 			outputFile.write("{}({})\n".format(key,value))	
-
+	
 
 
 def main():
 	
-	runQuery()
+	#Prompt and warn the user
+	call("cls", shell=True) #clear the terminal
+	print("\n********************************************************************************")
+	print("\nThis script will find the Hotfix IDs and the associated MS KnowledgeBase titles")
+	print("Please make sure that you have a working internet connection!\n")
+	question = raw_input("Proceed? Y/N...  ")
+	
+	#If yes, run the functions to collate updates
+	if question == "y":
+		
+		runQuery()
+		#use the results returned by runQuery in searchHTML
+		searchHTML(runQuery())
 			
-			
+	else:
+		exit()
+	
+	
 if __name__=="__main__":
 	main()
 
